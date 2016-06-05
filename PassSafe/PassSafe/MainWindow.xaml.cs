45382da073f056568c7e64211b968f98d8cdf402
Piokamie <piokamie@gmail.com>
2016-06-05 15:15:10 +0200

    Add files via upload

---
+++ PassSafe/PassSafe/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PassSafe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Subject> PasswordsList { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            PasswordsList = new ObservableCollection<Subject>();
+            Subject lol = new Subject("Piotras", "CHWDP", "wwww.pornhub.com", "JEBAC OLICJE");
+            PasswordsList.Add(lol);
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddDialogue.IsOpen = true;
+        }
+        private void PopupCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddDialogue.IsOpen = false;
+        }
+
+        private void PopupAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordsList.Add(new Subject(LoginTextBox.Text, PasswordTextBox.Text, 
+                URLTextBox.Text, NotesTextBox.Text));
+            LoginTextBox.Clear();
+            PasswordTextBox.Clear();
+            URLTextBox.Clear();
+            NotesTextBox.Clear();
+            AddDialogue.IsOpen = false;
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Subject item in PasswordsList)
+            {
+              item.Encrypt();
+                ListView1.Items.Refresh();
+
+                
+            }
+           
+        }
+    }
+}
---
+++ PassSafe/PassSafe/Subject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassSafe
+{
+    public class Subject
+    {
+        
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string URL { get; set; }
+        public string Notes { get; set; }
+
+        public Subject(string Login, string Password, string URL, string Notes)
+        {
+            this.Login = Login;
+            this.Password = Password;
+            this.URL = URL;
+            this.Notes = Notes;
+        }
+
+        public void Encrypt()
+        {
+            this.Login = "#Encrypted";
+            this.Password = "#Encrypted";
+            this.URL = "#Encrypted";
+            this.Notes = "#Encrypted";
+        }
+        public void Decrypt()
+        {
+            this.Login = "#Decrypted";
+            this.Password = "#Decrypted";
+            this.URL = "#Decrypted";
+            this.Notes = "#Decrypted";
+        }
+    }
+}
